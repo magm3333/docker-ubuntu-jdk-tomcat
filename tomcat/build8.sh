399981f537c88e53b8300b12e44aae3299f2e67e
Mariano García Mattío <magm3333@gmail.com>
2020-06-30 11:22:51 -0300

    Mejoras build tomcat

--- tomcat/build8.sh
+++ tomcat/build8.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
 VERSION_P=8
 VERSION_S=5.56
-docker build --build-arg VER_P=$VERSION_P --build-arg VER_S=$VERSION_S -t magm3333/tomcat:$VERSION_P.$VERSION_S .
+JDK_VERSION=8
+docker build --build-arg JDK_VER=$JDK_VERSION --build-arg VER_P=$VERSION_P --build-arg VER_S=$VERSION_S -t magm3333/tomcat:$VERSION_P.$VERSION_S .
--- tomcat/build9.sh
+++ tomcat/build9.sh
@@ -2,4 +2,5 @@
 
 VERSION_P=9
 VERSION_S=0.36
-docker build --build-arg VER_P=$VERSION_P --build-arg VER_S=$VERSION_S -t magm3333/tomcat:$VERSION_P.$VERSION_S .
+JDK_VERSION=11
+docker build --build-arg JDK_VER=$JDK_VERSION --build-arg VER_P=$VERSION_P --build-arg VER_S=$VERSION_S -t magm3333/tomcat:$VERSION_P.$VERSION_S .
