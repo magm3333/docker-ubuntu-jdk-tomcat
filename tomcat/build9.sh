550678087bf94b85f2214552334383266b434431
magm3333 <magm3333@gmail.com>
2022-08-03 09:25:58 -0300

    add TZ,curl to jdk & update to tomcat 9.0.65

--- tomcat/build9.sh
+++ tomcat/build9.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 VERSION_P=9
-VERSION_S=0.36
+VERSION_S=0.65
 JDK_VERSION=11
 docker build --build-arg JDK_VER=$JDK_VERSION --build-arg VER_P=$VERSION_P --build-arg VER_S=$VERSION_S -t magm3333/tomcat:$VERSION_P.$VERSION_S .
