dc8fb344803c996b68a72ee533f38d50daf1632c
magm3333 <magm3333@gmail.com>
2023-02-17 14:27:50 -0300

    add TZ and tomcat user

--- tomcat/build9.sh
+++ tomcat/build9.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 VERSION_P=9
-VERSION_S=0.65
+VERSION_S=0.71
 JDK_VERSION=11
 docker build --build-arg JDK_VER=$JDK_VERSION --build-arg VER_P=$VERSION_P --build-arg VER_S=$VERSION_S -t magm3333/tomcat:$VERSION_P.$VERSION_S .
